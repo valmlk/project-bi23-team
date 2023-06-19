0744c49433676b97031db0c71de9b7ea76de3275
Olha-mj <olga.sas.jasmins@gmail.com>
2023-06-19 23:46:41 +0300

    modal window and swiper
    Підключила свайпер
    Закінчила модальне вікно
    Ще оформлюю фотки

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,49 +1,44 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    speed: 400,
-    spaceBetween: 100,
-  
-    // If we need pagination
+var swiper = new Swiper(".mySwiper", {
+
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
+      el: ".swiper-pagination",
+      clickable: true,
     },
-  
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-  // Default parameters
-  slidesPerView: 1,
-  spaceBetween: 10,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 40
-    }
-  }
-  
   });
-  
-  
-  
-  
+
+
+
+
+//   Swiper for reviews section
+
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 16
+          
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 16
+          
+        },
+        // when window width is >= 1200px
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 28
+          
+        }
+      }
+
+  });
